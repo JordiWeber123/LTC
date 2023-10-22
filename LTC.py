9f932deadcdb5876802cadf4c4bd4d120f2a9010
Newsplayer <luisavc204@gmail.com>
2023-10-21 23:36:48 -0500

    Added the segmentation for the data in order to train and test on different partitions.

--- LTC.py
+++ LTC.py
@@ -9,16 +9,31 @@ from keras.utils import pad_sequences
 categories = ["Toys & Games", "Home & Kitchen", "Clothing, Shoes & Jewelry", "Sports & Outdoors", "Baby Products", "Arts, Crafts & Sewing", "Office Products", "Hobbies", "Industrial & Scientific", "Health & Household", "Remote & App Controlled Vehicle Parts", "Tools & Home Improvement", "Remote & App Controlled Vehicles & Parts", "Pet Supplies", "Patio, Lawn & Garden", "Grocery & Gourmet Food", "Beauty & Personal Care", "Automotive", "Electronics", "Video Games", "Musical Instruments", "Movies & TV", "Cell Phones & Accessories"]
 
 # Load the Excel file with titles and categories
-input_file = "C://Users/luisa/Coding/HackTX23/e_commerce_input.xlsx"
-output_file = "C://Users/luisa/Coding/HackTX23/e_commerce_output.xlsx"
+input_file = "e_commerce_input.xlsx"
+output_file = "e_commerce_output.xlsx"
 
 # Load the dataset from the Excel file
 df = pd.read_excel(input_file)
 
+# Split the original data into only the first category in "Category"
 for i, row in enumerate(df["Category"]):
      df["Category"][i] = str(row).split(" |")[0]
+
+# Delete the "nan" rows
 df = df[df.Category != "nan"]
+
+# Avoid oversampling with Toys & Games
 df = df.drop(df[df["Category"] == "Toys & Games"].sample(frac=.80).index)
+
+# Creating a dataframe with 50%
+# values of original dataframe
+part_50 = df.sample(frac = 0.5)
+ 
+# Creating dataframe with 
+# rest of the 50% values
+rest_part_50 = df.drop(part_50.index)
+
+df = part_50
 
 titles = df['Product Name'].values
 category_labels = df['Category'].apply(categories.index).values
@@ -27,14 +42,14 @@ category_labels = df['Category'].apply(categories.index).values
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(titles)
 X = tokenizer.texts_to_sequences(titles)
-X = pad_sequences(X, maxlen=10)  # Pad sequences to a fixed length
+X = pad_sequences(X, maxlen=50)  # Pad sequences to a fixed length
 
 # Convert categories to one-hot encoding
 y = keras.utils.to_categorical(category_labels, num_classes=len(categories))
 
 # Build and compile the neural network model
 model = keras.Sequential([
-    keras.layers.Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=8, input_length=10),
+    keras.layers.Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=8, input_length=50),
     keras.layers.Flatten(),
     keras.layers.Dense(16, activation='relu'),
     keras.layers.Dense(len(categories), activation='softmax')
@@ -44,6 +59,8 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 
 # Train the model
 model.fit(X, y, epochs=20, verbose=1)
+
+df = rest_part_50
 
 # Predict categories and confidence scores
 predictions = model.predict(X)
@@ -58,3 +75,7 @@ df['Confidence'] = confidence_scores
 df.to_excel(output_file, index=False)
 
 print(f"Predictions saved to {output_file}")
+
+# todo: check if the columns are identical
+
+#print(f"Total Correct:", correctRatio)
