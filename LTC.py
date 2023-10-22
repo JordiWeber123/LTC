fdb6b99a787cbcc502bc192d159f165f29455420
Newsplayer <luisavc204@gmail.com>
2023-10-22 04:36:33 -0500

    New Model Using Naive Bayes

--- LTC.py
+++ LTC.py
@@ -1,16 +1,11 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from keras.preprocessing.text import Tokenizer
-from keras.utils import pad_sequences
-
-# Sample categories for classification
-categories = ["Toys & Games", "Home & Kitchen", "Clothing, Shoes & Jewelry", "Sports & Outdoors", "Baby Products", "Arts, Crafts & Sewing", "Office Products", "Hobbies", "Industrial & Scientific", "Health & Household", "Remote & App Controlled Vehicle Parts", "Tools & Home Improvement", "Remote & App Controlled Vehicles & Parts", "Pet Supplies", "Patio, Lawn & Garden", "Grocery & Gourmet Food", "Beauty & Personal Care", "Automotive", "Electronics", "Video Games", "Musical Instruments", "Movies & TV", "Cell Phones & Accessories"]
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
 
 # Load the Excel file with titles and categories
 input_file = "e_commerce_input.xlsx"
-output_file = "e_commerce_output.xlsx"
 
 # Load the dataset from the Excel file
 df = pd.read_excel(input_file)
@@ -23,62 +18,20 @@ for i, row in enumerate(df["Category"]):
 df = df[df.Category != "nan"]
 
 # Avoid oversampling with Toys & Games
-df = df.drop(df[df["Category"] == "Toys & Games"].sample(frac=.80).index)
+df = df.drop(df[df["Category"] == "Toys & Games"].sample(frac=.50).index)
 
-# Creating a dataframe with 50%
-# values of original dataframe
-part_50 = df.sample(frac = 0.5)
- 
-# Creating dataframe with 
-# rest of the 50% values
-rest_part_50 = df.drop(part_50.index)
 
-df = part_50
+X = df['Product Name']
+y = df['Category']
 
-titles = df['Product Name'].values
-category_labels = df['Category'].apply(categories.index).values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a tokenizer and preprocess the text
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(titles)
-X = tokenizer.texts_to_sequences(titles)
-X = pad_sequences(X, maxlen=10)  # Pad sequences to a fixed length
+tfidf_vectorizer = TfidfVectorizer(max_features=100)  # Adjust the number of features as needed
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
 
-# Convert categories to one-hot encoding
-y = keras.utils.to_categorical(category_labels, num_classes=len(categories))
+classifier = MultinomialNB()
+classifier.fit(X_train_tfidf, y_train)
 
-# Build and compile the neural network model
-model = keras.Sequential([
-    keras.layers.Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=8, input_length=10),
-    keras.layers.Flatten(),
-    keras.layers.Dense(16, activation='relu'),
-    keras.layers.Dense(len(categories), activation='softmax')
-])
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Train the model
-model.fit(X, y, epochs=50, verbose=1)
-
-df = rest_part_50
-
-# Predict categories and confidence scores
-predictions = model.predict(X)
-predicted_categories = [categories[np.argmax(pred)] for pred in predictions]
-confidence_scores = [round(max(pred), 2) for pred in predictions]
-
-# Add the predicted categories and confidence scores to the DataFrame
-df['Predicted_Category'] = predicted_categories
-df['Confidence'] = confidence_scores
-
-# Save the DataFrame with predictions to a new Excel file
-df.to_excel(output_file, index=False)
-
-print(f"Predictions saved to {output_file}")
-
-match = len(df[df["Category"] == df["Predicted_Category"]])
-count = len(df["Category"])
-correctRatio = (match / count)
-     
-print("Total Correct:", match)
-print("Correct Percentage:", correctRatio)
+y_pred = classifier.predict(X_test_tfidf)
+print(classification_report(y_test, y_pred))
