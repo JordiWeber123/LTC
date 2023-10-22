6da014824fe81ff2491bd8a5dc03852b6b474f6e
JordiWeber <a01028249@itesm.mx>
2023-10-22 06:26:28 -0500

    Finalized model and new data set

--- LTC.py
+++ LTC.py
@@ -1,84 +1,51 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from keras.preprocessing.text import Tokenizer
-from keras.utils import pad_sequences
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
 
-# Sample categories for classification
-categories = ["Toys & Games", "Home & Kitchen", "Clothing, Shoes & Jewelry", "Sports & Outdoors", "Baby Products", "Arts, Crafts & Sewing", "Office Products", "Hobbies", "Industrial & Scientific", "Health & Household", "Remote & App Controlled Vehicle Parts", "Tools & Home Improvement", "Remote & App Controlled Vehicles & Parts", "Pet Supplies", "Patio, Lawn & Garden", "Grocery & Gourmet Food", "Beauty & Personal Care", "Automotive", "Electronics", "Video Games", "Musical Instruments", "Movies & TV", "Cell Phones & Accessories"]
+# Set the input csv file to processed Amazon listings
+input_file = 'combined_data.csv'
 
-# Load the Excel file with titles and categories
-input_file = "e_commerce_input.xlsx"
-output_file = "e_commerce_output.xlsx"
+# Create the pandas dataframe from the given csv file
+df = pd.read_csv(input_file)
 
-# Load the dataset from the Excel file
-df = pd.read_excel(input_file)
+# x is the NAME / PRODUCT / TITLE / LISTING (x is assumed to be the first column)
+x = df['name']
+# y is the CATEGORY of the corresponding product (y is assumed to be the second column)
+y = df['main_category']
 
-# Split the original data into only the first category in "Category"
-for i, row in enumerate(df["Category"]):
-     df["Category"][i] = str(row).split(" |")[0]
+# Trains with half the data, and uses the rest to validate and test
+# Used for demonstration purposes, otherwise, you can allow the model to train with the whole csv file.
+x_train, x_test, y_train, y_test = train_test_split(
+    
+    x, y, test_size=0.5, random_state=42)
 
-# Delete the "nan" rows
-df = df[df.Category != "nan"]
+tfidf_vectorizer = TfidfVectorizer(max_features=5000)
+x_train_tfidf = tfidf_vectorizer.fit_transform(x_train)
+x_test_tfidf = tfidf_vectorizer.transform(x_test)
 
-# Avoid oversampling with Toys & Games
-df = df.drop(df[df["Category"] == "Toys & Games"].sample(frac=.80).index)
+classifier = MultinomialNB()
+classifier.fit(x_train_tfidf, y_train)
 
-# Creating a dataframe with 50%
-# values of original dataframe
-part_50 = df.sample(frac = 0.5)
- 
-# Creating dataframe with 
-# rest of the 50% values
-rest_part_50 = df.drop(part_50.index)
+# Predicts the test cases, and runs a classification report to demo results
+y_pred = classifier.predict(x_test_tfidf)
+print(classification_report(y_test, y_pred))
 
-df = part_50
+# Logic for allowing the user to categorize their own products / titles
+while True:
+user_input = input('Enter a product title (or type "exit" to quit): ')
 
-titles = df['Product Name'].values
-category_labels = df['Category'].apply(categories.index).values
+    if user_input.lower() == 'exit':
+        break
 
-# Create a tokenizer and preprocess the text
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(titles)
-X = tokenizer.texts_to_sequences(titles)
-X = pad_sequences(X, maxlen=10)  # Pad sequences to a fixed length
+    # Preprocess the user input
+    user_input_tfidf = tfidf_vectorizer.transform([user_input])
 
-# Convert categories to one-hot encoding
-y = keras.utils.to_categorical(category_labels, num_classes=len(categories))
+    # Predict category and display confidence score
+    predicted_category = classifier.predict(user_input_tfidf)[0]
+    confidence_score = classifier.predict_proba(user_input_tfidf).max()
 
-# Build and compile the neural network model
-model = keras.Sequential([
-    keras.layers.Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=8, input_length=10),
-    keras.layers.Flatten(),
-    keras.layers.Dense(16, activation='relu'),
-    keras.layers.Dense(len(categories), activation='softmax')
-])
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Train the model
-model.fit(X, y, epochs=50, verbose=1)
-
-df = rest_part_50
-
-# Predict categories and confidence scores
-predictions = model.predict(X)
-predicted_categories = [categories[np.argmax(pred)] for pred in predictions]
-confidence_scores = [round(max(pred), 2) for pred in predictions]
-
-# Add the predicted categories and confidence scores to the DataFrame
-df['Predicted_Category'] = predicted_categories
-df['Confidence'] = confidence_scores
-
-# Save the DataFrame with predictions to a new Excel file
-df.to_excel(output_file, index=False)
-
-print(f"Predictions saved to {output_file}")
-
-match = len(df[df["Category"] == df["Predicted_Category"]])
-count = len(df["Category"])
-correctRatio = (match / count)
-     
-print("Total Correct:", match)
-print("Correct Percentage:", correctRatio)
+    # Print to console
+    print(f'Predicted Category: {predicted_category}')
+    print(f'Confidence Score: {confidence_score:.2f}\n')
